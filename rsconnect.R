72071ebf8a844136a7d70c13d69581fc34f0b3c2
zhihu xu <154651548+zhihu3456@users.noreply.github.com>
2025-02-21 15:21:20 +1100

    Add files via upload

---
+++ rsconnect.R
@@ -0,0 +1,6 @@
+library(rsconnect)
+rsconnect::setAccountInfo(name='qr5g6g-zhihu-xu',
+                          token='A362330C28F6C23724686276D32F153D',
+                          secret='IMfwHUEYX4pLCL2VytxBZMazu/egPCFiTcbQfLU+')
+rsconnect::deployApp("G:/论文/AU-HWproj/AU-HWproj/HwShiny")
+
